77e9368ddd7706c753cc39d59b650710a09ac021
mathter <mathter@mail.ru>
2026-08-13 00:44:27 +0300

    Make Dsl.origin return immutable PathMap

--- core/src/main/scala/io/github/mathter/morph/dsl/base/OriginSourceEval.scala
+++ core/src/main/scala/io/github/mathter/morph/dsl/base/OriginSourceEval.scala
@@ -5,5 +5,5 @@ import io.github.mathter.morph.dsl.{Dsl, Source}
 import io.github.mathter.morph.eval.{Context, Tracer}
 
 private class OriginSourceEval(implicit dsl: Dsl, tracer: Tracer) extends AbstractEval[PathMap] with Source[PathMap] {
-  override def evalI(context: Context): Opt[PathMap] = Opt(context.origin)
+  override def evalI(context: Context): Opt[PathMap] = Opt(context.origin.asImmutable)
 }
