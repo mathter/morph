411dde8db272cc4b82c0de50ba2855ce2db328f2
mathter <mathter@mail.ru>
2026-08-30 15:23:14 +0300

    docs: add license header to all java and scala files

--- core/src/main/scala/io/github/mathter/morph/conv/DateTimeConv.scala
+++ core/src/main/scala/io/github/mathter/morph/conv/DateTimeConv.scala
@@ -1,3 +1,18 @@
+// Copyright (c) 2026 mathter
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, without
+// warranties or condition of any kind. You may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// All rights reserved.
 package io.github.mathter.morph.conv
 
 import java.time.format.DateTimeFormatter.{ISO_LOCAL_DATE, ISO_LOCAL_DATE_TIME, ISO_OFFSET_DATE, ISO_OFFSET_DATE_TIME, ISO_OFFSET_TIME, ISO_ZONED_DATE_TIME}
